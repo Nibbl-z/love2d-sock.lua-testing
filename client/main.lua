327bdc495f36db3b116c5ee15b5a84a915062ad4
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-07-07 17:37:42 -0600

    shooting bullets

--- client/main.lua
+++ client/main.lua
@@ -1,54 +1,65 @@
 sock = require("sock")
 
 local world = nil
-local environment = nil
+local bullets = nil
+local enemies = nil
 local index = 0
 
 function love.load()
     client = sock.newClient("localhost", 22122)
-    
+
     client:on("connect", function(data)
         print("Client connected to the server.")
     end)
-    
+
     client:on("disconnect", function(data)
         print("Client disconnected from the server.")
     end)
-    
-    client:on("getPlayers", function (data)
-        world = data
+
+    client:on("getGame", function (data)
+        world = data[1]
+        bullets = data[2]
     end)
-    
+
     client:on("updatePlayers", function(msg)
-        print(msg[1], msg[2])
         if tonumber(msg[1]) ~= index and world ~= nil then
             print("moving "..msg[1].. "!!")
             world[msg[1]] = msg[2]
         end
     end)
+    
+    client:on("updateBullets", function (data)
+        bullets = data
+    end)
 
     client:on("getIndex", function (data)
         index = data
     end)
-    
+
     client:connect()
-    
+
 end
 
 local movementDirections = {
 	a = -1, d = 1
 }
 
+function love.keypressed(key)
+    if key == "space" then
+        client:send("shoot")
+    end
+end
+
 function love.update(dt)
-    
-    if world == nil then
-        client:send("getPlayers")
+
+    if world == nil or bullets == nil then
+        client:send("getGame")
     else
         local plr = world[tostring(index)]
 
         for k, v in pairs(movementDirections) do
             if love.keyboard.isDown(k) then
-                
+
                 if plr ~= nil then
                     plr.x = plr.x + v * 10
                     client:send("move", plr.x)
@@ -60,11 +71,8 @@ function love.update(dt)
     if index == 0 then
         client:send("getIndex")
     end
-    
-    
+
     client:update()
-    
-    
 end
 
 function love.draw()
@@ -73,12 +81,12 @@ function love.draw()
             love.graphics.rectangle("fill", player.x, player.y, 50, 50)
         end
     end
-	
     
-    if environment ~= nil then
-        for _, v in ipairs(environment) do
-            love.graphics.rectangle("fill", v.pX, v.pY, v.sX, v.sY)
+    
+    if bullets ~= nil then
+        for _, v in ipairs(bullets) do
+            love.graphics.rectangle("fill", v.x, v.y, 5, 20)
         end
     end
-    
+
 end
--- server/main.lua
+++ server/main.lua
@@ -3,7 +3,9 @@ instance = require("yan.instance.instance")
 
 local playersInstances = {}
 local players = {}
-local environment = {}
+local bullets = {}
+local bulletsInstances = {}
+local enemies = {}
 
 function love.load()
     server = sock.newServer("*", 22122)
@@ -32,10 +34,24 @@ function love.load()
         plr.Position.X = data
     end)
     
-    server:on("getPlayers", function (data, client)
-        client:send("getPlayers", players)
+    server:on("getGame", function (data, client)
+        client:send("getGame", {players, bullets})
     end)
 
+    server:on("shoot", function (data, client)
+        local bullet = instance:New(nil, "Bullet"..tostring(#bullets))
+        local plr = playersInstances[tostring(client:getIndex())]
+
+        bullet.Position.X = plr.Position.X
+        bullet.Position.Y = plr.Position.Y
+
+        table.insert(bulletsInstances, bullet)
+        table.insert(bullets, {
+            x = bullet.Position.X + 25,
+            y = bullet.Position.Y
+        })
+    end)
+    
     print("server is up and running")
 end
 
@@ -47,6 +63,24 @@ function love.update(dt)
         }
         
         server:sendToAll("updatePlayers", {k, players[k]})
+    end
+
+    for i, bullet in ipairs(bulletsInstances) do
+        bullet.Position.Y = bullet.Position.Y - 10
+        
+        bullets[i] = {
+            x = bullet.Position.X,
+            y = bullet.Position.Y
+        }
+    end
+    
+    server:sendToAll("updateBullets", bullets)
+    
+    for i, bullet in ipairs(bulletsInstances) do
+        if bullet.Position.Y < -100 then
+            table.remove(bulletsInstances, i)
+            table.remove(bullets, i)
+        end
     end
     
     server:update()
