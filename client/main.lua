5b4afd0847d510422e84ffb984954158b36e8caa
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-07-07 22:30:19 -0600

    added menu to put in ip to join

--- client/main.lua
+++ client/main.lua
@@ -7,6 +7,12 @@ local scores = {}
 local index = 0
 local health = 100
 
+local screen = require("yan.instance.ui.screen")
+local textinput = require("yan.instance.ui.textinput")
+local textbutton = require("yan.instance.ui.textbutton")
+local label = require("yan.instance.ui.label")
+
+local client = nil
 local sprites = {
     Player = "player.png",
     Enemy = "enemy.png",
@@ -20,6 +26,80 @@ local sounds = {
     Explosion = {"explosion.wav", "static"},
     Music = {"music.mp3", "stream"}
 }
+
+local connecting = false
+local connectWaiting = false
+local connectStartTimer = 0
+
+function connect()
+    connectWaiting = false
+    connecting = true
+    client = sock.newClient(ipInput.Text, 22122)
+    
+    local status, err = pcall(function() 
+        client:connect()
+    end)
+    
+    print(status, err)
+
+    if status == false then
+        connecting = false
+        ipInput.Text = ""
+        statusText.Text = "Failed to connect"
+        client = nil
+    else
+        menu.Enabled = false
+        connecting = false
+        client:on("connect", function(data)
+            print("Client connected to the server.")
+        end)
+        
+        client:on("disconnect", function(data)
+            print("Client disconnected from the server.")
+        end)
+        
+        client:on("getGame", function (data)
+            world = data[1]
+            bullets = data[2]
+            enemies = data[3]
+            health = data[4]
+        end)
+
+        client:on("updatePlayers", function(msg)
+            if tonumber(msg[1]) ~= index and world ~= nil then
+                world[msg[1]] = msg[2]
+            end
+        end)
+        
+        client:on("updateBullets", function (data)
+            bullets = data
+        end)
+
+        client:on("updateEnemies", function (data)
+            enemies = data
+        end)
+
+        client:on("getIndex", function (data)
+            print("Data", data)
+            index = data
+        end)
+
+        client:on("updateScores", function (data)
+            scores = data
+        end)
+        
+        client:on("updateHealth", function (data)
+            health = data
+        end)
+        
+        client:on("sfx", function (data)
+            sounds[data]:clone():play()
+        end)
+
+        
+    end
+end
+    
 
 function love.load()
     font = love.graphics.newFont("/fonts/PressStart2P-Regular.ttf", 32)
@@ -35,94 +115,97 @@ function love.load()
     sounds.Music:setLooping(true)
     sounds.Music:play()
 
-    client = sock.newClient("localhost", 22122)
-
-    client:on("connect", function(data)
-        print("Client connected to the server.")
-    end)
     
-    client:on("disconnect", function(data)
-        print("Client disconnected from the server.")
-    end)
 
-    client:on("getGame", function (data)
-        world = data[1]
-        bullets = data[2]
-        enemies = data[3]
-        health = data[4]
-    end)
+    menu = screen:New(nil)
 
-    client:on("updatePlayers", function(msg)
-        if tonumber(msg[1]) ~= index and world ~= nil then
-            world[msg[1]] = msg[2]
-        end
-    end)
+    ipInput = textinput:New(nil, menu, "Enter server IP", 32)
+    ipInput:SetPosition(0.5,0,0.5,0)
+    ipInput:SetSize(0.5,0,0.2,0)
+    ipInput:SetColor(0,1,0,1)
+    ipInput:SetTextColor(1,1,1,1)
+    ipInput:SetAnchorPoint(0.5,0.5)
+    ipInput:SetPlaceholderTextColor(0,0,0,1)
+
+    statusText = label:New(nil, menu, "", 32, "center")
+    statusText:SetPosition(0.5,0,0.7,0)
+    statusText:SetSize(0.5,0,0.2,0)
+    statusText:SetColor(0,1,0,1)
+    statusText:SetAnchorPoint(0.5,0.5)
+
+    menu.Enabled = true
     
-    client:on("updateBullets", function (data)
-        bullets = data
-    end)
+    ipInput.MouseDown = function ()
+        ipInput:SetColor(0,0.8,0,1)
+    end
+    
+    ipInput.MouseEnter = function ()
+        ipInput:SetColor(0,0.6,0,1)
+    end
+    
+    ipInput.MouseLeave = function ()
+        ipInput:SetColor(0,1,0,1)
+    end
 
-    client:on("updateEnemies", function (data)
-        enemies = data
-    end)
-
-    client:on("getIndex", function (data)
-        print("Data", data)
-        index = data
-    end)
-
-    client:on("updateScores", function (data)
-        scores = data
-    end)
-
-    client:on("updateHealth", function (data)
-        health = data
-    end)
-
-    client:on("sfx", function (data)
-        sounds[data]:clone():play()
-    end)
-
-    client:connect()
-
+    ipInput.OnEnter = function ()
+        statusText.Text = "Connecting..."
+        connectWaiting = true
+        connectStartTimer = love.timer.getTime() + 0.2
+    end
 end
 
 local movementDirections = {
 	a = -1, d = 1
 }
 
-function love.keypressed(key)
-    if key == "space" then
+function love.keypressed(key, scancode, rep)
+    ipInput:KeyPressed(key, scancode, rep)
+    
+    if key == "space" and client ~= nil then
         client:send("shoot")
         sounds.Shoot:clone():play()
     end
 end
 
+function love.textinput(t)
+    ipInput:TextInput(t)
+end
+
 function love.update(dt)
+    if connectWaiting and love.timer.getTime() > connectStartTimer then
+        connect()
+    end
 
-    if world == nil or bullets == nil then
-        client:send("getGame")
-    else
-        local plr = world[tostring(index)]
-
-        for k, v in pairs(movementDirections) do
-            if love.keyboard.isDown(k) then
-                if plr ~= nil then
-                    plr.x = plr.x + v * 10
-                    client:send("move", plr.x)
+    if client ~= nil then
+        if world == nil or bullets == nil then
+            client:send("getGame")
+        else
+            local plr = world[tostring(index)]
+    
+            for k, v in pairs(movementDirections) do
+                if love.keyboard.isDown(k) then
+                    if plr ~= nil then
+                        plr.x = plr.x + v * 10
+                        client:send("move", plr.x)
+                    end
                 end
             end
         end
+        
+        if index == 0 then
+            client:send("getIndex")
+        end
+        
+        client:update()
+    else
+        menu:Update()
     end
     
-    if index == 0 then
-        client:send("getIndex")
-    end
-    
-    client:update()
 end
 
 function love.draw()
+    menu:Draw()
+
     love.graphics.setColor(1,1,1,1)
     if world ~= nil then
         for k, player in pairs(world) do
@@ -153,19 +236,23 @@ function love.draw()
         totalScore = totalScore + v
     end
     
-    love.graphics.setFont(font)
-    love.graphics.setColor(0,1,0,1)
-    love.graphics.printf(tostring(totalScore), 0, 10, love.graphics.getWidth(), "center")
+    if world ~= nil then
+        love.graphics.setFont(font)
+        love.graphics.setColor(0,1,0,1)
+        love.graphics.printf(tostring(totalScore), 0, 10, love.graphics.getWidth(), "center")
 
-    love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 25, (health / 100) * love.graphics.getWidth(), 25)
+        love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 25, (health / 100) * love.graphics.getWidth(), 25)
 
-    if health <= 0 then
-        love.graphics.setColor(1,0,0,0.5)
-        love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-        
-        love.graphics.setColor(1,1,1,1)
-        love.graphics.printf("GAME OVER", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+        if health <= 0 then
+            love.graphics.setColor(1,0,0,0.5)
+            love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+            
+            love.graphics.setColor(1,1,1,1)
+            love.graphics.printf("GAME OVER", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+        end
     end
+
+    
     
     --love.graphics.print("Health: "..tostring(health), 0, 40)
 end
